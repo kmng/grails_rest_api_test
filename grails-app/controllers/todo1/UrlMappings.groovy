cdc0c99d7d49e9b0dfe5fdfe686175ab6f3e5579
Stephen <kmng@ucla.edu>
2017-09-14 10:47:15 -0700

    update url mapping to put everything to /api/

--- grails-app/controllers/todo1/UrlMappings.groovy
+++ grails-app/controllers/todo1/UrlMappings.groovy
@@ -3,17 +3,17 @@ package todo1
 class UrlMappings {
 
     static mappings = {
-        delete "/$controller/$id(.$format)?"(action:"delete")
-        get "/$controller(.$format)?"(action:"index")
-        get "/$controller/$id(.$format)?"(action:"show")
-        post "/$controller(.$format)?"(action:"save")
-        put "/$controller/$id(.$format)?"(action:"update")
-        patch "/$controller/$id(.$format)?"(action:"patch")
+        delete "/api/$controller/$id(.$format)?"(action:"delete")
+        get "/api/$controller(.$format)?"(action:"index")
+        get "/api/$controller/$id(.$format)?"(action:"show")
+        post "/api/$controller(.$format)?"(action:"save")
+        put "/api/$controller/$id(.$format)?"(action:"update")
+        patch "/api/$controller/$id(.$format)?"(action:"patch")
 
         "/"(controller: 'application', action:'index')
         "500"(view: '/error')
         "404"(view: '/notFound')
-        "/todos"(resources:"todo")
-        "/pendingTodos"(controller: 'todo', action: 'pending')
+        "/api/todos"(resources:"todo")
+        "/api/pendingTodos"(controller: 'todo', action: 'pending')
     }
 }
