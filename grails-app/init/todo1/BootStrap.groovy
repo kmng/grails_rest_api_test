2ed8e7486412954be348710f97e2f587ddd89e8f
Stephen <kmng@ucla.edu>
2017-09-21 16:18:43 -0700

    fixed authentication issue
    need to manuelly encode the password, the user object cannot load spring security class

--- grails-app/init/todo1/BootStrap.groovy
+++ grails-app/init/todo1/BootStrap.groovy
@@ -2,10 +2,11 @@ package todo1
 
 class BootStrap {
 
+  def springSecurityService
   def init = { servletContext ->
      5.times { new Todo(description: "Todo ${it+1}").save() }
      def role1 = new Role(authority:"ROLE_USER").save flush:true
-     def user1 = new User(username:"user1",password:"pwd1").save flush:true
+     def user1 = new User(username:"user1",password:springSecurityService.encodePassword('pwd1')).save flush:true
 
      UserRole.create user1, role1, true
      
